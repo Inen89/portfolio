94975454328d094259c3b66a7962d7af581c120a
Grzegorz Łoś <grzeglos@gmail.com>
2024-09-01 19:54:09 +0200

    Update config files
    - Updated .eslintrc.cjs
    - Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#081b29",
+        aqua: "#00E5FF",
+        blue: "#005059",
+        red: "#FF5733",
+        green: "#234519",
+        lime: "#39FF14",
+        yellow: "#FDCC49",
+        lemon: "#FFFF00",
+        grey: "#EDEDED",
+        "deep-blue": "#081b29",
+      },
+
+      // eslint-disable-next-line no-unused-vars
+      backgroundImage: (theme) => ({
+        "gradient-blue-green":
+          "linear-gradient(90deg, #04D9FF 45%, #39FF14 90.73%)",
+      }),
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
