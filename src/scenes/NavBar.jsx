8ec24d31c4c611ebe409892c262162a77cf67cd6
Inen89 <grzeglos@gmail.com>
2025-02-22 12:36:39 +0100

    update navbar for better responsiveness on smaller screens: - NavBar.jsx - updated styles for mobile layout - Fixed spacing and alignment issues on smaller screens

--- src/scenes/NavBar.jsx
+++ src/scenes/NavBar.jsx
@@ -3,10 +3,12 @@ import Link from "../components/Link";
 export default function Navbar({ selectedPage, setSelectedPage, isTopOfPage }) {
   const navBackground = isTopOfPage ? "bg-blue" : "bg-deep-blue";
   return (
-    <nav className={`${navBackground} w-full fixed top-0 py-6 z-40`}>
-      <div className="flex items-center justify-between mx-auto w-5/6">
-        <div className="text-3xl font-opensans font-bold">GŁ</div>
-        <div className="flex gap-16 font-poppins font-semibold">
+    <nav
+      className={`${navBackground} w-full fixed top-0 p-2 sm:px-0 sm:py-6 z-40`}
+    >
+      <div className="flex items-center justify-between mx-auto w-11/12 sm:w-5/6">
+        <div className="text-lg sm:text-3xl font-opensans font-bold">GŁ</div>
+        <div className="flex gap-4 sm:gap-16 font-poppins font-semibold text-sm sm:text-base">
           <Link
             page="Home"
             selectedPage={selectedPage}
