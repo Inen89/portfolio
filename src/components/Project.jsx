9179c0b97ca5ed0948684fbcf98c4083575b621f
Inen89 <grzeglos@gmail.com>
2025-02-23 12:06:35 +0100

    update Projects section for better RWD on smaller screens: - Projects.jsx - fixed styling for better grid control on different resolutions - Project.jsx - fixed styling to improve readability on smaller screens

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,29 +11,31 @@ export default function Project({
       ? "opacity-100 hover:opacity-100"
       : "opacity-0 hover:opacity-100";
 
-  const overlayStyles = `absolute min-h-60 h-full w-full ${opacity}  transition duration-500
-    bg-gray-400 z-30 flex flex-col justify-center items-center text-center p-4 px-3 text-deep-blue `;
+  const overlayStyles = `absolute  h-full w-full ${opacity}  transition duration-500
+    bg-gray-400 z-30 flex flex-col justify-center items-center text-center  p-2 px-2 text-deep-blue`;
 
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   const Wrapper = href ? "a" : "button"; // Dynamiczny wybór elementu
   return (
     <Wrapper
-      className="relative w-full h-full max-w-full aspect-[1/0.8] border-transparent outline-2 hover:outline-magenta hover:shadow-neon-pink hover:animate-pulse-once"
+      className="relative w-full h-full  max-w-[400px] max-h-[400px] aspect-[1/0.8] border-transparent outline-2 hover:outline-magenta hover:shadow-neon-pink hover:animate-pulse-once"
       href={href || undefined} // href tylko, jeśli jest podane
       target={href ? "_blank" : undefined} // Otwieraj w nowej karcie, jeśli href jest
       rel={href ? "noopener noreferrer" : undefined} // Dla bezpieczeństwa
     >
-      <div className="relative w-full h-full max-w-full aspect-[1/0.8] border-transparent outline-2 hover:outline-magenta  hover:shadow-neon-pink hover:animate-pulse-once ">
+      <div className="relative w-full h-fit max-w-full aspect-[1/0.8] border-transparent outline-2 hover:outline-magenta  hover:shadow-neon-pink hover:animate-pulse-once ">
         <div className={overlayStyles}>
           <p className="text-2xl font-playfair">{title}</p>
-          <p className="my-4 overflow-y-auto px-2 ">{summary}</p>
+          <p className="my-4 overflow-y-auto px-2 text-sm md:text-base ">
+            {summary}
+          </p>
           <p className="text-wrap w-full ">
             {technologies && technologies.length > 0
               ? technologies.map((technology) => (
                   <span
                     key={technology}
-                    className="bg-yellow p-1 px-2 mx-1 mb-2 rounded-md inline-block hover:bg-magenta hover:shadow-neon-pink-sm hover:text-grey"
+                    className="bg-yellow p-1 px-2 mx-1 mb-2 rounded-md inline-block text-xs md:text-sm hover:bg-magenta hover:bg-opacity-30 hover:shadow-neon-pink-sm hover:text-black"
                   >
                     {technology}
                   </span>
--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -3,19 +3,26 @@ import projectsData from "../assets/data/projects.json";
 
 import portfolioImg from "../assets/img/portfolio.jpg";
 import smartGuysImg from "../assets/img/smart-guys.jpg";
+import LineGradient from "../components/LineGradient";
 
 export default function Projects() {
   return (
-    <section id="projects" className="pt-48 pb-48">
+    <section id="projects" className="pt-12 pb-12 sm:pt-48 sm:pb-48">
       {/* HEADINGS */}
-      <div className="w-7/12 mx-auto text-center">
+      <div className=" w-11/12 sm:w-7/12 mx-auto text-center">
         <div>
-          <p className="font-playfair font-semibold text-6xl">
+          <p className="font-playfair font-semibold text-5xl sm:text-6xl">
             <span className="text-magenta">PRO</span>JECTS
           </p>
-          <div className="flex justify-center mt-5"></div>
+          <div className="flex justify-center mt-5">
+            {" "}
+            <LineGradient
+              width="w-9/12 sm:w-10/12 md:w-7/12"
+              gradientColor="bg-gradient-magenta-white"
+            />
+          </div>
         </div>
-        <p className="mt-10 mb-20">
+        <p className="mt-5 mb-10 sm:mt-10 sm:mb-20">
           On this page, you'll find selected projects that showcase my
           experience and skills in web application development. Each one has
           been created with passion and attention to detail, using modern
@@ -27,11 +34,11 @@ export default function Projects() {
 
       {/* PROJECTS */}
       <div className="flex justify-center">
-        <div className="grid grid-cols-3 gap-1 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 ">
           {/* ROW 1 */}
           <div
             className="flex justify-center text-center items-center p-10 bg-orange
-              w-full h-full text-2xl font-playfair font-semibold border-2 border-transparent"
+              w-full h-full  max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold border-2 border-transparent"
           >
             BEAUTIFUL USER INTERFACES
           </div>
@@ -61,7 +68,7 @@ export default function Projects() {
           />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
-              w-full h-full text-2xl font-playfair font-semibold border-0 border-transparent"
+              w-full h-full  max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold border-2 border-transparent "
           >
             SMOOTH USER EXPERIENCE
           </div>
