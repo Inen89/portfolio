e29222ad9d8cac2725f7105dfd85c961eca2b606
Inen89 <grzeglos@gmail.com>
2025-02-28 19:11:30 +0100

    feat: Update URL with anchor links
    - Updated Link.jsx to include section addresses in the URL
    - When navigating through the navbar, the URL now reflects the current section's ID

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,13 +3,18 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 export default function Link({ page, selectedPage, setSelectedPage }) {
   const lowerCasePage = page.toLowerCase();
 
+  // update URL addresss
+  const handleClick = () => {
+    window.history.pushState(null, "", `#${lowerCasePage}`);
+    setSelectedPage(lowerCasePage);
+  };
   return (
     <AnchorLink
       className={`${
         selectedPage === lowerCasePage ? "text-aqua" : ""
       } hover:text-aqua transition duration-500`}
       href={`#${lowerCasePage}`}
-      onClick={() => setSelectedPage(lowerCasePage)}
+      onClick={handleClick}
     >
       {page}
     </AnchorLink>
